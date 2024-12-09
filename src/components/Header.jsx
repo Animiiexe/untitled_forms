52229f2515b22eca5cccc4851ac2bcdf468612c9
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2024-12-09 14:25:44 +0000

    react-font

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return <header className="flex gap-2 items-centre p-2">
+    <img src="favicon.png" className="w-10" />
+    <p className="text-lg font-semibold">Untitled UI</p>
+  </header>;
+}
+
+export default Header
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// console.log('Hello world')
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
+
+const appEl = document.querySelector("#app");
+const root = createRoot(appEl);
+
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+);
+
